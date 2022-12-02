87f782274d4e99b91bcf1edd107c79dbf5298011
Dawid Roszman <d.roszman.682@studms.ug.edu.pl>
2022-12-02 16:13:13 +0100

    added ts version for day solution

---
+++ day1/a.ts
@@ -0,0 +1,20 @@
+const input = await Deno.readTextFile("./input.txt");
+const lines = input.split("\n");
+
+const calculateMaxCaloriesForN = (n: number) => {
+  const sums: number[] = [];
+  let current: number[] = [];
+  lines.forEach((line) => {
+    if (line == "") {
+      sums.push(current.reduce((partialSum, a) => partialSum + a, 0));
+      current = [];
+      return;
+    }
+    current.push(parseInt(line));
+  });
+  sums.sort((a, b) => b - a);
+  return sums.slice(0, n);
+};
+
+const res = calculateMaxCaloriesForN(3);
+console.log(res);
