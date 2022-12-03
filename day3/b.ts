1040a9f7590ef35f50f4ad54422562d0c74dcd3b
Dawid Roszman <d.roszman.682@studms.ug.edu.pl>
2022-12-03 19:34:05 +0100

    formated file

--- day3/b.ts
+++ day3/b.ts
@@ -1,26 +1,29 @@
-const text  = await Deno.readTextFile("./input.prod.txt")
+const text = await Deno.readTextFile("./input.prod.txt");
 
-
-let current : string[] = []
-const lines = text.split("\n").slice(0, -1).map((line, idx) => {
-  current.push(line)
-  if((idx+1)%3 == 0){
-    const ans = [...current]
-    current = []
-    return ans
-  }
-}).filter(n => n != undefined)
+let current: string[] = [];
+const lines = text
+  .split("\n")
+  .slice(0, -1)
+  .map((line, idx) => {
+    current.push(line);
+    if ((idx + 1) % 3 == 0) {
+      const ans = [...current];
+      current = [];
+      return ans;
+    }
+  })
+  .filter((n) => n != undefined);
 
 const out = lines.map((line) => {
-  const first_line = line![0].split("")
-  const second_line = line![1].split("")
-  const third_line = line![2].split("")
-  const same_char = first_line.find(n => second_line.includes(n) && third_line.includes(n))
+  const first_line = line![0].split("");
+  const second_line = line![1].split("");
+  const third_line = line![2].split("");
+  const same_char = first_line.find(
+    (n) => second_line.includes(n) && third_line.includes(n)
+  );
   if (same_char == same_char?.toUpperCase())
-    return same_char!.charCodeAt(0)-38
-  else
-    return same_char!.charCodeAt(0)-96
-})
+    return same_char!.charCodeAt(0) - 38;
+  else return same_char!.charCodeAt(0) - 96;
+});
 
 console.log(out.reduce((sum, a) => sum + a));
-
