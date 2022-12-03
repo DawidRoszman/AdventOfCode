9af1f7ec24a02cf93e245071aa040275c2fb8ac0
Dawid Roszman <d.roszman.682@studms.ug.edu.pl>
2022-12-03 16:36:38 +0100

    shortened code for day3 b

--- day3/b.ts
+++ day3/b.ts
@@ -1,19 +1,20 @@
 const text  = await Deno.readTextFile("./input.prod.txt")
-const lines = text.split("\n").slice(0, -1)
-const three_lines : string[][] = []
+
+
 let current : string[] = []
-lines.forEach((line, idx) => {
+const lines = text.split("\n").slice(0, -1).map((line, idx) => {
   current.push(line)
   if((idx+1)%3 == 0){
-    three_lines.push(current)
+    const ans = [...current]
     current = []
+    return ans
   }
-})
+}).filter(n => n != undefined)
 
-const out = three_lines.map((line) => {
-  const first_line = line[0].split("")
-  const second_line = line[1].split("")
-  const third_line = line[2].split("")
+const out = lines.map((line) => {
+  const first_line = line![0].split("")
+  const second_line = line![1].split("")
+  const third_line = line![2].split("")
   const same_char = first_line.find(n => second_line.includes(n) && third_line.includes(n))
   if (same_char == same_char?.toUpperCase())
     return same_char!.charCodeAt(0)-38
